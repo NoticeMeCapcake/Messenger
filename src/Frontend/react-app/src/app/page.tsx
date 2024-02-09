42ed594d7160f981ed2a931205c2fed4ffe3017b
NoticeMeCapcake <danko3033@gmail.com>
2024-02-09 22:47:41 +0300

    some adjustments

--- src/Frontend/react-app/src/app/page.tsx
+++ src/Frontend/react-app/src/app/page.tsx
@@ -1,16 +1,15 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DialogueArea from "@/components/dialogues/DialogueArea"; // Import bootstrap CSS
-
-const myAction = (tag: string) => console.log(tag);
 
 export default async function Home() {
   return (
     <main className="container-fluid bg-black">
       <div className="row">
         <div className="col-3 bg-dark" style={{height: "100vh"}}>
-          <DialogueArea clickAction={myAction}></DialogueArea>
+          <DialogueArea clickAction={(tag: string): void => console.log(tag)}></DialogueArea>
         </div>
         <div className="col-9 bg-primary">
           Item2
--- src/Frontend/react-app/src/components/dialogues/DialogueArea.tsx
+++ src/Frontend/react-app/src/components/dialogues/DialogueArea.tsx
@@ -4,13 +4,17 @@ import * as ScrollArea from '@radix-ui/react-scroll-area';
 import "./style.css";
 
 const TAGS = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`);
-const DialogueArea = (clickAction: (tag: string) => void) => (
+
+type Props = {
+    clickAction: (tag: string) => void
+}
+const DialogueArea = (props: Props) => (
     <ScrollArea.Root className="ScrollAreaRoot">
         <ScrollArea.Viewport className="ScrollAreaViewport">
             <div style={{ padding: '15px 20px' }}>
                 <div className="Text">Tags</div>
                 {TAGS.map((tag) => (
-                    <div className="Tag hover" key={tag} onClick={() => clickAction(tag)}>
+                    <div className="Tag hover" key={tag} onClick={() => props.clickAction(tag)}>
                         {tag}
                     </div>
                 ))}
