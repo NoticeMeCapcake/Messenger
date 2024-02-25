1b35efe14ffaf8760c24e046681ac4a346d01056
NoticeMeCapcake <danko3033@gmail.com>
2024-02-26 02:50:28 +0300

    message service init

---
+++ src/Backend/MessageControlService/src/main/java/mess/messagecontrolservice/MessageControlServiceApplication.java
@@ -0,0 +1,13 @@
+package mess.messagecontrolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageControlServiceApplication.class, args);
+	}
+
+}
---
+++ src/Backend/MessageControlService/src/test/java/mess/messagecontrolservice/MessageControlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mess.messagecontrolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageControlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
